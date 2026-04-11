dae8e2a26ce25fc8a9b58fd8f05e70f07593b531
notayalink <ayalinkmtg@gmail.com>
2026-04-11 12:23:58 -0500

    ai v1

--- scenes/levels/enemyai.gd
+++ scenes/levels/enemyai.gd
@@ -1,18 +1,24 @@
 extends CharacterBody3D
 
-var astar = get_parent().astar
+@onready var astar = get_parent().astar
 @onready var player = $"../../charcontrol"
-
+const MOVE_TIME = 0.5
+var movetimer = MOVE_TIME
+var pointpath = []
 
 func _process(delta: float) -> void:
-	
+	_handle_ai_move(delta)
 
 func _handle_ai_move(delta):
 	if movetimer > 0:
 		movetimer -= delta
 	else:
 		var playerpos = astar.get_closest_point(player.global_position)
-		var mypos = astar.getclosest_point(global_position)
-		var nextpoint = astar.get_point_path(mypos, playerpos)[1]
+		var mypos = astar.get_closest_point(global_position)
+		
+		if astar.get_point_path(mypos, playerpos):
+			pointpath = astar.get_point_path(mypos, playerpos)[1]
+			global_position = pointpath
+			movetimer = MOVE_TIME
 		
 		
