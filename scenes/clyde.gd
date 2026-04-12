0d4c1f78ee0683964b24d48e03f8549d3d933b2d
johnwdodge <71807808+johnwdodge@users.noreply.github.com>
2026-04-12 09:28:09 -0500

    asd

--- scenes/clyde.gd
+++ scenes/clyde.gd
@@ -3,9 +3,12 @@ extends CharacterBody3D
 @onready var astar = get_parent().astar
 @onready var player = $"../../charcontrol"
 @onready var MOVE_TIME = get_parent().MOVE_TIME
-@onready var spawn = $"../../Pellet_Altar"
 @onready var movetimer = MOVE_TIME
+@onready var altars: Node3D = $"../../Altars"
+@onready var pellet_altar: Node3D = $"../../Altars/Pellet_Altar"
+
 var pointpath = []
+
 
 func _process(delta: float) -> void:
 	_handle_ai_move(delta)
@@ -20,7 +23,7 @@ func _handle_ai_move(delta):
 		if astar.get_point_path(mypos, playerpos):
 			pointpath = astar.get_point_path(mypos, playerpos)
 			if pointpath.size() < 8 and pointpath.size() > 3:
-				pointpath = astar.get_point_path(mypos, astar.get_closest_point(spawn.global_position))
+				pointpath = astar.get_point_path(mypos, astar.get_closest_point(pellet_altar.global_position))
 			if pointpath.size() > 1:
 				global_position = pointpath[1]
 			movetimer = MOVE_TIME
